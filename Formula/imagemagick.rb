9c16f8e9efbc445fdacd792b7bb28752c2d50ad4
Ricardo Rodrigues (Freir) <ricardojoaofreitas@gmail.com>
2022-03-03 14:47:34 +0000

    Add ImageMagick Formula @7.0.11-13

---
+++ Formula/imagemagick.rb
@@ -0,0 +1,98 @@
+class Imagemagick < Formula
+  desc "Tools and libraries to manipulate images in many formats"
+  homepage "https://www.imagemagick.org/"
+  url "https://www.imagemagick.org/download/releases/ImageMagick-7.0.11-13.tar.xz"
+  sha256 "11797503bc4dd72c7e65e60783e156fd74ec0a551d2f1fe1f41eaa4bd06987a5"
+  license "ImageMagick"
+  revision 1
+  head "https://github.com/ImageMagick/ImageMagick.git"
+
+  livecheck do
+    url "https://download.imagemagick.org/ImageMagick/download/"
+    regex(/href=.*?ImageMagick[._-]v?(\d+(?:\.\d+)+-\d+)\.t/i)
+  end
+
+  bottle do
+    sha256 arm64_big_sur: "00e47167fb272aff8ff1aba4889d84b9f2e86847bef76fe0d64095bcf4222aac"
+    sha256 big_sur:       "4a3e7623371785e763e36984abd091874e966487c25a1731feaa7b817be4ba7c"
+    sha256 catalina:      "28206d9f9926e67e5bdd79e4ab3bf07a717d309b7fcf84eb296693423c10ac73"
+    sha256 mojave:        "b51a38de60be313476f3828a6b6b419ea610b39f84d6dd5132700462438e4b7d"
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "freetype"
+  depends_on "ghostscript"
+  depends_on "jpeg"
+  depends_on "libheif"
+  depends_on "liblqr"
+  depends_on "libomp"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "libtool"
+  depends_on "little-cms2"
+  depends_on "openexr"
+  depends_on "openjpeg"
+  depends_on "webp"
+  depends_on "xz"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "libxml2"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libx11"
+  end
+
+  skip_clean :la
+
+  def install
+    # Avoid references to shim
+    inreplace Dir["**/*-config.in"], "@PKG_CONFIG@", Formula["pkg-config"].opt_bin/"pkg-config"
+
+    args = %W[
+      --enable-osx-universal-binary=no
+      --prefix=#{prefix}
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --disable-opencl
+      --enable-shared
+      --enable-static
+      --with-freetype=yes
+      --with-gvc=no
+      --with-modules
+      --with-openjp2
+      --with-openexr
+      --with-webp=yes
+      --with-heic=yes
+      --with-gslib
+      --with-gs-font-dir=#{HOMEBREW_PREFIX}/share/ghostscript/fonts
+      --with-lqr
+      --without-fftw
+      --without-pango
+      --without-wmf
+      --enable-openmp
+      ac_cv_prog_c_openmp=-Xpreprocessor\ -fopenmp
+      ac_cv_prog_cxx_openmp=-Xpreprocessor\ -fopenmp
+      LDFLAGS=-lomp\ -lz
+    ]
+
+    on_macos do
+      args << "--without-x"
+    end
+
+    # versioned stuff in main tree is pointless for us
+    inreplace "configure", "${PACKAGE_NAME}-${PACKAGE_BASE_VERSION}", "${PACKAGE_NAME}"
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_match "PNG", shell_output("#{bin}/identify #{test_fixtures("test.png")}")
+    # Check support for recommended features and delegates.
+    features = shell_output("#{bin}/convert -version")
+    %w[Modules freetype jpeg png tiff].each do |feature|
+      assert_match feature, features
+    end
+    assert_match "Helvetica", shell_output("#{bin}/identify -list font")
+  end
+end
